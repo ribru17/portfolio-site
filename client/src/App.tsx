98d1e9571c303573dbbfa94c8bddfbd4d2e20df3
Riley B <ribru17@hotmail.com>
2022-05-31 02:05:11 -0700

    beautifying navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,7 @@ import {
 import Home from './Components/Pages/Home'
 import Contact from './Components/Pages/Contact'
 import Projects from './Components/Pages/Projects'
+import Experience from './Components/Pages/Experience'
 import Menu from './Components/SVGs/Menu'
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
           <Link id="nameLink" to="/">Riley Bruins</Link>
           <div id="otherLinks">
             <Link to="/projects">Projects</Link>
+            <Link to="/experience">Experience</Link>
             <Link to="/contact">Contact</Link>
           </div>
           <Menu fill='white' id="menuSvg" onClick={() => {setIsClicked(!isClicked)}}/>
@@ -31,6 +33,7 @@ function App() {
           <Route path="/contact" element={<Contact />}></Route>
           <Route path="/" element={<Home />}></Route>
           <Route path="/projects" element={<Projects />}></Route>
+          <Route path="/experience" element={<Experience />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
---
+++ client/src/Components/Pages/Experience.tsx
@@ -0,0 +1,8 @@
+export default function Experience() {
+    return (
+        <>
+            <h1>Experience</h1>
+            <p>3 Years JS ...</p>
+        </>
+    )
+}

77eeecc3c1b3a91aeb543d350af8c8aed55de82b
Riley B <ribru17@hotmail.com>
2022-09-16 19:25:28 -0700

    fixing border transitions by swapping them with box-shadow

--- src/Components/Pages/PageNotFound.tsx
+++ src/Components/Pages/PageNotFound.tsx
@@ -1,27 +1,16 @@
-/* eslint-disable react/jsx-no-comment-textnodes */
 import { Link } from "react-router-dom"
 import "./CSS/pagenotfound.css"
 
 export default function PageNotFound() {
 
-//     const snippet =
-// `1| <div className="pageDiv">
-// 2|
-// 3|    {/* TODO: implement this page */}
-// 4|
-// 5| </div>`;
-
     return (
         <div className="pageDiv active">
             <h1>Page not found :(</h1>
-            <Link id="notFoundHomeLink" to="/">Home</Link>
+            <Link id="notFoundHomeLink" to="/">Return home</Link>
             <div className="codeSnippet">
                 <header>
                     PageNotFound.tsx
                 </header>
-                {/* <pre>
-                    {snippet}
-                </pre> */}
                 <pre>{"<div className=\"pageDiv\">"}</pre>
                 <pre className="cursorLine">{"    {/* TODO: implement this page */}"}</pre>
                 <pre>{"</div>"}</pre>
